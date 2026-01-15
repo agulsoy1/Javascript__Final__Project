d55572b0ff0d55d671768219add8bf8181615b63
agulsoy1 <alexandretg99@gmail.com>
2026-01-14 23:02:16 -0800

    changed the primary function

--- movie.js
+++ movie.js
@@ -36,34 +36,64 @@ function showLoading() {
   loading.classList += " loading__state";
 }
 
-async function searchMovies(event) {
+// async function searchMovies(event) {
+//   const searchTerm = searchBar.value;
+//   const response = await fetch(
+//     `https://www.omdbapi.com/?apikey=6d4005a9&s=${searchTerm}`
+//   );
+//   movieData = await response.json();
+// //   console.log(movieData);
+//   movieDisplayELem.innerHTML = movieData.Search.map((movie) =>
+//     displayMovies(movie)
+//   ).join("");
+// }
+
+// function displayMovies(movie) {
+//   movie = `<div class="movie__display">
+//                     <figure class="movie__poster--wrapper">
+//                         <img src="${movie.Poster}" class="movie__poster" alt="">
+//                     </figure>
+//                     <div class="movie__info">
+//                         <h3 class="movie__title">${movie.Title}</h3>
+//                         <div class="movie__year">${movie.Year}</div>
+//                     </div>
+//                 </div>`;
+//   return movie;
+// }
+
+async function searchMovies() {
+  //     const movieDisplayContainer = document.querySelector('.row');
   const searchTerm = searchBar.value;
   const response = await fetch(
     `https://www.omdbapi.com/?apikey=6d4005a9&s=${searchTerm}`
   );
-  movieData = await response.json();
-  console.log(movieData);
-  movieDisplayELem.innerHTML = movieData.Search.map((movie) =>
-    displayMovies(movie)
-  ).join("");
-}
+  const data = await response.json();
 
-function displayMovies(movie) {
-  movie = `<div class="movie__display">
-                    <figure class="movie__poster--wrapper">
-                        <img src="${movie.Poster}" class="movie__poster" alt="">
-                    </figure>
-                    <div class="movie__info">
-                        <h3 class="movie__title">${movie.Title}</h3>
-                        <div class="movie__year">${movie.Year}</div>
-                    </div>
-                </div>`;
-  return movie;
+  // Clear previous results
+  document.querySelector(".movie__display").remove(); // remove template
+
+  // Create a card for each movie
+  data.Search.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie__display", "movie-card");
+    movieCard.setAttribute("data-year", movie.Year); // store year for filtering
+
+    movieCard.innerHTML = `
+                <figure class="movie__poster--wrapper">
+                    <img src="${movie.Poster}" class="movie__poster" alt="">
+                </figure>
+                <div class="movie__info">
+                    <h3 class="movie__title">${movie.Title}</h3>
+                    <div id="movie__year">${movie.Year}</div>
+                </div>
+            `;
+    console.log(movieCard);
+    movieDisplayELem.appendChild(movieCard);
+  });
 }
 
 function fetchData() {
   showLoading();
-  movieDisplayELem.classList += " movie__display--hidden";
   setTimeout(() => {
     movieDisplayELem.classList.remove("movie__display--hidden");
     loading.classList += " loading__state--hidden";
@@ -75,72 +105,74 @@ function showHomePage() {
   window.location.href = `${window.location.origin}/index.html#`;
 }
 
-optionSelect.addEventListener('change', async (event) => {
-    const data = await searchMovies();
-    const year = data.Search[0].Year;
+optionSelect.addEventListener("change", async (event) => {
+  const data = await searchMovies();
+  const selectedOption = event.target.value;
+  const movieCards = document.querySelectorAll(".movie__display");
+  movieCards.forEach((card) => {
+    const year = parseInt(card.getAttribute("data-year"));
     console.log(year);
-    const selectedOption = event.target.value;
     console.log(selectedOption);
-    
-    switch(selectedOption){
-        case 'None':
-            movieDisplayELem.style.display = 'block';
-            break;
-        case '1950s':
-            if(1950 <= year && year <= 1959){
-                movieDisplayELem.style.display = 'block';
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '1960s':
-            if(1960 <= year && year <= 1969){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '1970s':
-            if(1970 <= year && year <= 1979){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '1980s':
-            if(1980 <= year && year <= 1989){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '1990s':
-            if(1990 <= year && year <= 1999){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '2000s':
-            if(2000 <= year && year <= 2009){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '2010s':
-            if(2010 <= year && year <= 2019){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
-        case '2020s':
-            if(2020 <= year && year <= 2024){
-                movieDisplayELem.style.display = 'block';    
-            }else{
-                movieDisplayELem.style.display = 'none';
-            }
-            break;
+    switch (selectedOption) {
+      case "None":
+        card.style.display.style.display = "block";
+        break;
+      case "1950s":
+        if (1950 <= year && year <= 1959) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1960s":
+        if (1960 <= year && year <= 1969) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1970s":
+        if (1970 <= year && year <= 1979) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1980s":
+        if (1980 <= year && year <= 1989) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1990s":
+        if (1990 <= year && year <= 1999) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "2000s":
+        if (2000 <= year && year <= 2009) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "2010s":
+        if (2010 <= year && year <= 2019) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "2020s":
+        if (2020 <= year && year <= 2024) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
     }
-})
+  });
+});
