ad7f41fe9212999d79a634d46b4f62eff88b73de
agulsoy1 <alexandretg99@gmail.com>
2026-01-21 02:55:09 -0800

    Finished final touches

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const homeSearchBtn = document.querySelector(".header__search--icon");
 const homeSearchBar = document.querySelector(".header__search--bar");
 
-homeSearchBtn.addEventListener('click', function(event){
-    if(event.type === "click"){
-        event.preventDefault();
-        showMovieBrowser();
-    }
-});
+if(homeSearchBtn){   
+    homeSearchBtn.addEventListener('click', function(event){
+        if(event.type === "click"){
+            event.preventDefault();
+            showMovieBrowser();
+        }
+    });
+}
 
-homeSearchBar.addEventListener('keydown', function(event){
-    if(event.key === "Enter"){
-        event.preventDefault();
-        showMovieBrowser();
-    }
-});
-
+if(homeSearchBar){
+    homeSearchBar.addEventListener('keydown', function(event){
+        if(event.key === "Enter"){
+            event.preventDefault();
+            showMovieBrowser();
+        }
+    });
+}
+    
 function showMovieBrowser(){
-    const homeSearchTerm = homeSearchBar.value;
+    const homeSearchTerm = homeSearchBar ? homeSearchBar.value: "";
     localStorage.setItem("query", homeSearchTerm);
     window.location.href = `${window.location.origin}/movie.html#`;
 }
